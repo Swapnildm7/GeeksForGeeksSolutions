5e4863976e8a670488d97871595f1ca918b46e7b
anirudhkannan <anirudhkannan.v16@iiits.in>
2019-02-26 15:09:22 +0530

    Add files via upload

---
+++ search-a-node-in-bst.cpp
@@ -0,0 +1,21 @@
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Node structure 
+struct node {
+    int data;
+   struct node * right, * left;
+};*/
+/*You are required to complete this method*/
+bool search(node* root, int x)
+{
+    //Your code here
+    if(root==NULL)return false;
+    if(root->data==x) return true;
+    
+    else if(root->data > x)
+    return search(root->left,x);
+    return search(root->right,x);
+    
+}
