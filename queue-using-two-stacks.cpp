8b2fdcd8c92de8129589b83a5a7cf6ab76ec0f1f
anirudhkannan <anirudhkannan.v16@iiits.in>
2019-03-07 14:51:59 +0530

    Add files via upload

---
+++ queue-using-two-stacks.cpp
@@ -0,0 +1,36 @@
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* The structure of the class is
+class StackQueue{
+private:   
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; */
+/* The method push to push element into the queue */
+void StackQueue :: push(int x)
+ {
+        s1.push(x);// Your Code
+ }
+/*The method pop which return the element poped out of the queue*/
+int StackQueue :: pop()
+{       
+        if(s1.empty())return -1;
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int temp=s2.top();
+        s2.pop();
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return temp;
+        // Your Code       
+}
