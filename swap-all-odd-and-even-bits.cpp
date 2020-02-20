d434596527b160540b36acef2fc01e97b757b475
anirudhkannan <anirudhkannan.v16@iiits.in>
2020-02-20 10:09:34 +0530

    Add files via upload

---
+++ swap-all-odd-and-even-bits.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<(((n&170)>>1)|((n&85)<<1))<<"\n";
+	}
+	
+	return 0;
+}
